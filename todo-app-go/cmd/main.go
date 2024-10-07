a8a8c35d8a7eb7b5b81b08d47788768e0af7cf02
Oğuzhan <oguzhankuzlukluoglu@gmail.com>
2024-10-08 02:55:05 +0300

    frame created

--- todo-app-go/cmd/main.go
+++ todo-app-go/cmd/main.go
@@ -1,20 +1,66 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "github.com/go-chi/chi/v5"
-    _ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
+var db *sql.DB
+
+func connectToDB() {
+	var err error
+	for {
+		dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME")
+		db, err = sql.Open("mysql", dsn)
+		if err == nil && db.Ping() == nil {
+			log.Println("Veritabanına başarıyla bağlanıldı!")
+			break
+		}
+		log.Println("Veritabanına bağlanılamadı, tekrar deneniyor...")
+		time.Sleep(5 * time.Second) 
+	}
+}
+
+func migrate() {
+	createTableQuery := `
+		CREATE TABLE IF NOT EXISTS todos (
+			id INT AUTO_INCREMENT PRIMARY KEY,
+			title VARCHAR(255) NOT NULL,
+			description TEXT,
+			completed BOOLEAN DEFAULT FALSE
+		);
+	`
+	_, err := db.Exec(createTableQuery)
+	if err != nil {
+		log.Fatalf("Tablo oluşturulamadı: %v", err)
+	}
+	log.Println("Todos tablosu başarıyla oluşturuldu!")
+}
+
 func main() {
-    r := chi.NewRouter()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 
-    // Sağlık kontrolü endpoint'i
-    r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("Uygulama çalışıyor!"))
-    })
+	connectToDB() 
+	defer db.Close()
 
-    log.Println("Server 8080 portunda çalışıyor...")
-    http.ListenAndServe(":8080", r)
+	migrate()
+
+	r := chi.NewRouter()
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Uygulama çalışıyor ve MySQL'e bağlı!"))
+	})
+
+	log.Println("Server " + os.Getenv("APP_PORT") + " portunda çalışıyor...")
+	http.ListenAndServe(":"+os.Getenv("APP_PORT"), r)
 }
--- todo-app-go/go.mod
+++ todo-app-go/go.mod
@@ -1,12 +1,11 @@
 module todo-app-go
 
-go 1.23.0
-
-toolchain go1.23.2
+go 1.22
 
 require (
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/go-sql-driver/mysql v1.8.1
+	github.com/joho/godotenv v1.5.1
 )
 
 require filippo.io/edwards25519 v1.1.0 // indirect
